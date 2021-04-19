0d907da5f1cfff0aea5517a70a4cad866d2456cc
Nethmini Karunarathne <nethminikarunarathne@gmail.com>
2021-04-19 15:16:23 +0530

    add Unity Project 3

---
+++ Assets/Scripts/spawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    private Vector3 spawnPos = new Vector3(25,0,0);
+    public GameObject obstaclePrefab;
+    private float startDelay =2;
+    private float Interval = 2;
+    private PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("spawnObstacle", startDelay, Interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void spawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+       
+    }
+}
